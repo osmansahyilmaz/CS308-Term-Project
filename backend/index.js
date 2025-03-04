b866cc65248bcb10cf41f9d9bedb1a5c918d7864
bartuarslan <bartu.arslan@sabanciuniv.edu>
2025-03-04 19:52:42 +0300

    SCRUM-44: Setup backend connection to PostgreSQL

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config(); // Load environment variables from .env
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+});
+
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch(err => console.error('Database connection error', err));
